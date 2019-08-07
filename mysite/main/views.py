fe101b1a552da9fc681ea1df8ba3457e64bdc455
worlak2 <worlak2@mail.ru>
2019-08-07 22:08:58 +0300

    Add to config and move to main

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 
-class MyView(TemplateView):
-    template_name = "index.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
+class MyView(TemplateView):
+    template_name = "main/index.html"
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     return context
