515b360ab6701215b8d07817a2cb9c7a136d5b72
amir <ganizhev.amir@mail.ru>
2019-08-07 21:53:50 +0300

    my comment

---
+++ mysite/main/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
+class MyView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
